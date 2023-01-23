c645617991d99f05f81867ddb781bf8a42e2e882
AmazingBread <kn20850@gmail.com>
2023-01-24 00:59:26 +0900

    add leaflet

---
+++ src/components/meridian/meridian.vue
@@ -0,0 +1,18 @@
+
+<template>
+	<div id="map"></div>
+</template>
+<script>
+import L from "leaflet";
+export default {
+	data() {
+		return {};
+	},
+	create(){},
+	mounted(){},
+	methods: {
+		
+	},
+};
+</script>
+<style src="./meridian.scss" lang="scss" />
